87da01b9624beb9d16830c4f5e583e547ecee85b
= <=>
2015-02-04 11:40:58 +0530

    second version completed.but not working for multiple digits.

--- rpn.c
+++ rpn.c
@@ -4,30 +4,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-//it will create stack for the elements of expression 2 3 +
-int operator(char op){
-	if(op == '+' || op == '-' || op == '*' || op == '/')
-		return 1;
-	return 0;
-}
-
-Stack stackElements(char *expression){
-	int length = strlen(expression);
-	int i ,*result,store[5],j=0;
-	Stack s = createStack();
-
-	for(i = 0;i<length;i++){
-		if(expression[i] == ' '){
-			continue;
-		}
-		if( isdigit(expression[i]) ){
-			result = malloc( sizeof(int));
-			*result = atoi(&expression[i]);
-			push(&s,result);
-		}
-	}
-	return s;
-}
 int calculator(int a,int b,char expression){
 	int result ;
 	switch(expression){
@@ -39,6 +15,35 @@ int calculator(int a,int b,char expression){
 	}
 	return result;
 }
+
+//it will create stack for the elements of expression 2 3 +
+int operator(char op){
+	if(op == '+' || op == '-' || op == '*' || op == '/')
+		return 1;
+	return 0;
+}
+
+Stack stackElements(char *expression){
+	int length = strlen(expression);
+	int i ,a,b,*result,store[5],j=0;
+	Stack s = createStack();
+
+	for(i = 0;i<length;i++){
+		if(expression[i] == ' '){
+			continue;
+		}
+		if( isdigit(expression[i]) ){
+			result = malloc( sizeof(int));
+			*result = atoi(&expression[i]);
+			push(&s,result);
+		}
+		else
+			if(operator(expression[i])){
+				break;
+			}
+	}
+	return s;
+}
 // got expression ,need to separate it char by char n then evaluation
 
 int evaluate(char *expression){
@@ -47,16 +52,24 @@ int evaluate(char *expression){
 	int length = strlen(expression);
 
 	for(i=0; i<length; i++){
+
+		if(isdigit(expression[i])){
+			result = malloc( sizeof(int));
+			*result = atoi(&expression[i]);
+			push(&s,result);
+			continue;
+		}
+
 		if(operator(expression[i])){
 
 			a = *(int*)pop(&s);
 			b = *(int*)pop(&s);	
-			
+
 			result = malloc( sizeof(int));
 			*result = calculator(a,b,expression[i]);
-
 			push(&s,result);
 		}
+
 	}		
 	return *result;
 }
--- rpnTest.c
+++ rpnTest.c
@@ -2,7 +2,6 @@
 #include "expr_assert.h"
 #include "string.h"
 
-int evaluate(char *expression); 
 void test_evaluate_will_simplify_the_operation(){
 	int result;
 	char *expression = "2 3 +";
@@ -27,10 +26,39 @@ void test_evaluate_works_fine_with_numbers_not_seperated_by_spaces (){
 	assertEqual(evaluate("5 4+"),9);
 }
 
-// // void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_3(){
-// // 	int result = evaluate("2 2 2 * 2 - 3 + + ");
-// // 	assertEqual(result, 7);
-// // }
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_3(){
+	int result = evaluate("2 2 2 * 2 - 3 + + ");
+	assertEqual(result, 7);
+}
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_2_2_2(){
+	int result = evaluate("2 2 2 2 2 * * 2 + + 2 - *");
+	assertEqual(result, 20);
+}	
+
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_2_2_2_2_2_2(){
+	int result = evaluate("2 2 - 2 2 2 * 2 - - -");
+	assertEqual(result, 0);
+}
+
+void test_evaluate_returns_the_result_of_postfix_complex_expression_of_5_1_2_4_3(){
+	int result = evaluate("5 1 2 + 4 * + 3 -");
+	assertEqual(result, 14);
+}
+
+// void test_evaluate_returns_the_result_with_errorValue_1_when_wrong_expression_isPassed(){
+// 	int result = evaluate("5 4 1 + 4 * + 3 5 -");
+// 	assertEqual(result.error, 1);
+// 	assertEqual(result, 0);
+// }
+
+// void test_evaluate_returns_the_result_with_error_having_1_for_less_number_of_operands(){
+// 	int result = evaluate("1 + - 4 * + 5 -");
+// 	assertEqual(result.error, 1);
+// }
+
+// void test_evaluate_returns_the_result_with_error_having_1_for_less_number_of_operators(){
+// 	int result = evaluate("1 3 5 4 * + 5 -");
+// 	assertEqual(result.error, 1);
 
 // void test_evaluate_returns_the_result_of_postfix_operation_for_multi_digit_operands (){
 // 	assertEqual(evaluate("20 4 *"),80);
